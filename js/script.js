aff30b1fabaa7aa612ac1520d79e9bb0a2eca923
Jonathan Ferreira <j0n4t@hotmail.com>
2021-08-25 18:16:27 -0300

    Adiciona botão de remoção e refatoramento de criação de elemento

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 const inputItem = document.getElementById('addItem');
 const keepList = document.querySelector('.list');
 
+function createCustomElement(tag, className = '', innerText = '') {
+  const element = document.createElement(tag);
+  element.className = className;
+  element.innerText = innerText;
+  return element;
+}
+
 function addItemElement(text) {
-  const itemElement = document.createElement('li');
-  itemElement.innerText = text;
+  const itemElement = createCustomElement('li', 'item');
+  const textElement = createCustomElement('span', 'item-text', text);
+  const clearButton = createCustomElement('button', 'remove-item-btn', 'x');
+  clearButton.addEventListener('click', (ev) => ev.target.parentElement.remove());
+  itemElement.appendChild(textElement);
+  itemElement.appendChild(clearButton);
   keepList.appendChild(itemElement);
 }
 
